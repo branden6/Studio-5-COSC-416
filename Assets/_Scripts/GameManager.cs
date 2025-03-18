f157df3be222239da4f94fa9e4a83390c9652d22
Fahad Aljahlan <fahadmaj@hotmail.com>
2025-03-17 22:48:18 -0700

    [A][C] Created LivesCounterUI prefab and added supporting scripts – prefab not yet added to scenes

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -1,10 +1,15 @@
 ﻿using UnityEngine;
+using System.Collections;
 
 public class GameManager : SingletonMonoBehavior<GameManager>
 {
     [SerializeField] private int maxLives = 3;
     [SerializeField] private Ball ball;
     [SerializeField] private Transform bricksContainer;
+    [SerializeField] private LivesCounterUI livesCounter;
+    [SerializeField] private GameObject gameOverScreen;
+
+
     private int currentBrickCount;
     private int totalBrickCount;
 
@@ -14,6 +19,8 @@ public class GameManager : SingletonMonoBehavior<GameManager>
         ball.ResetBall();
         totalBrickCount = bricksContainer.childCount;
         currentBrickCount = bricksContainer.childCount;
+        livesCounter.UpdateLives(maxLives);
+
     }
 
     private void OnDisable()
@@ -34,18 +41,40 @@ public class GameManager : SingletonMonoBehavior<GameManager>
         currentBrickCount--;
         Debug.Log($"Destroyed Brick at {position}, {currentBrickCount}/{totalBrickCount} remaining");
         ScoreManager.scoreManager.AddScore(); //call scoremanager to add score! 
-        if (currentBrickCount == 0) 
+        if (currentBrickCount == 0)
         {
             SceneHandler.Instance.LoadNextScene();
             ScoreManager.scoreManager.InitializeScore(ScoreManager.scoreManager.GetScore());
         }
     }
 
+
     public void KillBall()
     {
         maxLives--;
-        // update lives on HUD here
-        // game over UI if maxLives < 0, then exit to main menu after delay
+
+        // Animate the lives decrement on HUD
+        livesCounter.UpdateLives(maxLives);
+
+        // Game over condition
+        if (maxLives < 0)
+        {
+            StartCoroutine(GameOverSequence());
+            return;
+        }
+
+        // Reset ball normally
         ball.ResetBall();
     }
+    private IEnumerator GameOverSequence()
+    {
+        Time.timeScale = 0f;
+        gameOverScreen.SetActive(true); // activate your GameOver UI
+
+        yield return new WaitForSecondsRealtime(1.5f); // use Realtime due to timeScale = 0
+
+        Time.timeScale = 1f;
+        SceneHandler.Instance.LoadMenuScene();
+    }
+
 }
